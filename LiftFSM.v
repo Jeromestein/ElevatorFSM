fd62a9e5d5cd0f276ae47517ef67e3cd76c33034
Jerome <nietsemorej@gmail.com>
2021-03-03 17:58:58 +0800

    Wirte the outline of the FSM.

--- LiftFSM.v
+++ LiftFSM.v
@@ -1,6 +1,28 @@
 module LiftFSM (
-    ports
+    input clk, rst_n,
+    input wire [2:0] in,
+    output wire [1:0] out
 );
-    
-    
+    reg [2:0] state, nxt_state;
+
+    // input
+    parameter _1U = 3'b001, _2U = 3'b010, _3U = 3'b011
+              _2D = 3'b110, _3D = 3'b111, _4D = 3'b100;
+    // output
+    parameter UP = 2'b00, DOWN = 2'b01, STAY = 2'b10;
+
+    always @(posedge clk ) begin
+        // sync reset 
+        if(!rst_n)
+            State <= S0;
+        else
+            State <= Next;
+    end
+
+    always @(state, in) begin
+        
+        
+    end
+
+
 endmodule
