4146fc3fccc68c7d6e690c8ff449e4b010f8441a
Jerome <nietsemorej@gmail.com>
2021-03-04 16:58:15 +0800

    Add code blocks of "get input name"&"get output name"

--- LiftFSM_tb.v
+++ LiftFSM_tb.v
@@ -1,35 +1,62 @@
 `timescale 1ns/100ps
 
-module LiftFSM_tb (
-    
-);
+module LiftFSM_tb ();
+
     reg clk, rst_n;
     reg [2:0] in;
     wire [1:0] out;
 
-    // input-3bit
-    // [2]-UP:0, DOWN:1
-    parameter [2:0] _1U = 3'b001, _2U = 3'b010, _3U = 3'b011,
-                    _2D = 3'b110, _3D = 3'b111, _4D = 3'b100;
-                    // NO-INPUT = 3'b111,  ?
-
     // instantiate LiftFSM
     LiftFSM U1 (clk, rst_n, in, out);
 
+
     ////////////////////
-    /* get state name */
+    /* get name       */
     ////////////////////
-    // 3 bytes, 24 bits
-    reg [23:0] nxt_state_name, crt_state_name;
-    // state-4bit 
+
+    /* define name variables */
+    // state name, 3 bytes, 24 bits
+    reg [23:0] crt_state_name, nxt_state_name;
+    // input name, 3 bytes, 24 bits
+    reg [23:0] input_name;
+    // output name, 4 bytes, 32 bits
+    reg [31:0] output_name;
+
+    /* define name parameters */
+    // state parameters
     // [3]-idle:0, busy:1
     // [2]-UP:0, DOWN:1
     parameter [3:0] S1 = 4'b0001, S2 = 4'b0010, S3 = 4'b0011, S4 = 4'b0100,
                     S12 = 4'b1001, S21 = 4'b1101,
                     S23 = 4'b1010, S32 = 4'b1110,
                     S34 = 4'b1011, S43 = 4'b1111;
+    // input parameters
+    // [2]-UP:0, DOWN:1
+    parameter [2:0] _1U = 3'b001, _2U = 3'b010, _3U = 3'b011,
+                    _2D = 3'b110, _3D = 3'b111, _4D = 3'b100;
+                    // NO-INPUT = 3'b111,  ?
+    // output parameters
+    parameter [1:0] UP = 2'b00, DOWN = 2'b01, STAY = 2'b10;
                     
     always @(*) begin
+        // get current state name
+        case (U1.crt_state)
+            S1: crt_state_name = "S1"; 
+            S2: crt_state_name = "S2"; 
+            S3: crt_state_name = "S3"; 
+            S4: crt_state_name = "S4"; 
+
+            S12: crt_state_name = "S12"; 
+            S21: crt_state_name = "S21"; 
+            S23: crt_state_name = "S23"; 
+            S32: crt_state_name = "S32"; 
+            S34: crt_state_name = "S34"; 
+            S43: crt_state_name = "S43"; 
+
+            default: crt_state_name = "S1";
+        endcase
+        
+        // get next state name
         case (U1.nxt_state)
             S1: nxt_state_name = "S1"; 
             S2: nxt_state_name = "S2"; 
@@ -46,22 +73,29 @@ module LiftFSM_tb (
             default: nxt_state_name = "S1";
         endcase
 
-        case (U1.crt_state)
-            S1: crt_state_name = "S1"; 
-            S2: crt_state_name = "S2"; 
-            S3: crt_state_name = "S3"; 
-            S4: crt_state_name = "S4"; 
+        // get input name
+        case (in)
+            _1U: input_name = "_1U"; 
+            _2U: input_name = "_2U"; 
+            _3U: input_name = "_3U"; 
+            _2D: input_name = "_2D"; 
+            _3D: input_name = "_3D"; 
+            _4D: input_name = "_4D"; 
 
-            S12: crt_state_name = "S12"; 
-            S21: crt_state_name = "S21"; 
-            S23: crt_state_name = "S23"; 
-            S32: crt_state_name = "S32"; 
-            S34: crt_state_name = "S34"; 
-            S43: crt_state_name = "S43"; 
-
-            default: crt_state_name = "S1";
+            // no default input
+            //default: input_name = "_1U";
         endcase
-    end
+
+        // get output name
+        case (out)
+            UP: output_name = "UP"; 
+            DOWN: output_name = "DOWN"; 
+            STAY: output_name = "STAY"; 
+
+            default: output_name = "STAY";
+        endcase
+        
+    end    
 
 
     initial begin
