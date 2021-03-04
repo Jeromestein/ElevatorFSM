b626aaa8955d1abae510cd899711fae52fe4b187
Jerome <nietsemorej@gmail.com>
2021-03-04 16:06:13 +0800

    Add the "get state name" block in testbench file

--- LiftFSM_tb.v
+++ LiftFSM_tb.v
@@ -13,7 +13,56 @@ module LiftFSM_tb (
                     _2D = 3'b110, _3D = 3'b111, _4D = 3'b100;
                     // NO-INPUT = 3'b111,  ?
 
+    // instantiate LiftFSM
     LiftFSM U1 (clk, rst_n, in, out);
+
+    ////////////////////
+    /* get state name */
+    ////////////////////
+    // 3 bytes, 24 bits
+    reg [23:0] nxt_state_name, crt_state_name;
+    // state-4bit 
+    // [3]-idle:0, busy:1
+    // [2]-UP:0, DOWN:1
+    parameter [3:0] S1 = 4'b0001, S2 = 4'b0010, S3 = 4'b0011, S4 = 4'b0100,
+                    S12 = 4'b1001, S21 = 4'b1101,
+                    S23 = 4'b1010, S32 = 4'b1110,
+                    S34 = 4'b1011, S43 = 4'b1111;
+                    
+    always @(*) begin
+        case (U1.nxt_state)
+            S1: nxt_state_name = "S1"; 
+            S2: nxt_state_name = "S2"; 
+            S3: nxt_state_name = "S3"; 
+            S4: nxt_state_name = "S4"; 
+
+            S12: nxt_state_name = "S12"; 
+            S21: nxt_state_name = "S21"; 
+            S23: nxt_state_name = "S23"; 
+            S32: nxt_state_name = "S32"; 
+            S34: nxt_state_name = "S34"; 
+            S43: nxt_state_name = "S43"; 
+
+            default: nxt_state_name = "S1";
+        endcase
+
+        case (U1.crt_state)
+            S1: crt_state_name = "S1"; 
+            S2: crt_state_name = "S2"; 
+            S3: crt_state_name = "S3"; 
+            S4: crt_state_name = "S4"; 
+
+            S12: crt_state_name = "S12"; 
+            S21: crt_state_name = "S21"; 
+            S23: crt_state_name = "S23"; 
+            S32: crt_state_name = "S32"; 
+            S34: crt_state_name = "S34"; 
+            S43: crt_state_name = "S43"; 
+
+            default: crt_state_name = "S1";
+        endcase
+    end
+
 
     initial begin
         clk = 0;
